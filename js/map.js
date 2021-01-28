da16f0eefdea1016d3b7d758633a8fb1360dc912
marco <marcopettorali1@gmail.com>
2021-01-28 17:22:50 +0100

    Uploaded project on GitHUb

---
+++ js/map.js
@@ -0,0 +1,22 @@
+function main() {
+    document.addEventListener('keyup', function (e) {
+        handleKey(e.key);
+    })
+}
+
+function handleKey(key) {
+    switch (e.key) {
+        case 'w':
+
+            break;
+        case 'a':
+
+            break;
+        case 's':
+
+            break;
+        case 'd':
+            s
+            break;
+    }
+}
