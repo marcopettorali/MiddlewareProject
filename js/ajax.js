3fabb377a146f4fba93447c18bec6b16d2d1a97c
Marco Pettorali <marcopettorali1@gmail.com>
2021-01-29 16:00:15 +0100

    Implemented map graphics

--- js/ajax.js
+++ js/ajax.js
@@ -1,15 +1,31 @@
-const DEBUG = true
+const DEBUG_FLAG = true
+
+var DEBUG = {
+    "Graziano_x": 180,
+    "Graziano_y": 300,
+    "Silvano_x": 280,
+    "Silvano_y": 700,
+    "Mariano_x": 580,
+    "Mariano_y": 80
+};
 
 // requestForPlayerPositions() is periodically called to ask for other players' position
 function requestForPlayerPositions() {
-    if (DEBUG) {
+    if (DEBUG_FLAG) {
         var ret = {
             "players": [
-                { "player": "Graziano", "position_x": "180", "position_y": "300" },
-                { "player": "Silvano", "position_x": "280", "position_y": "700" },
-                { "player": "Mariano", "position_x": "580", "position_y": "80" }
+                { "player": "Graziano", "position_x": DEBUG.Graziano_x, "position_y": DEBUG.Graziano_y },
+                { "player": "Silvano", "position_x": DEBUG.Silvano_x, "position_y": DEBUG.Silvano_y },
+                { "player": "Mariano", "position_x": DEBUG.Mariano_x, "position_y": DEBUG.Mariano_y }
             ]
         };
+        DEBUG.Graziano_x += 5 * (Math.floor(Math.random() * 3) - 1)
+        DEBUG.Graziano_y += 5 * (Math.floor(Math.random() * 3) - 1)
+        DEBUG.Silvano_x += 5 * (Math.floor(Math.random() * 3) - 1)
+        DEBUG.Silvano_y += 5 * (Math.floor(Math.random() * 3) - 1)
+        DEBUG.Mariano_x += 5 * (Math.floor(Math.random() * 3) - 1)
+        DEBUG.Mariano_y += 5 * (Math.floor(Math.random() * 3) - 1)
+
         drawPlayers(ret.players);
         return;
     }
--- js/map.js
+++ js/map.js
@@ -1,5 +1,7 @@
-const AJAX_POLLING_PERIOD_MS = 1000
+const AJAX_POLLING_PERIOD_MS = 500
 const PLAYER_PAWN_SIZE = 50
+
+var PLAYER_FOLLOWER = null;
 
 function main() {
     document.addEventListener('keyup', function (e) {
@@ -33,25 +35,36 @@ function drawPlayer(player) {
         'width: ' + PLAYER_PAWN_SIZE + 'px;' +
         'top: ' + player.position_y + 'px;' +
         'left: ' + player.position_x + 'px;' +
+        'margin-top: ' + (-PLAYER_PAWN_SIZE) + 'px;' +
+        'margin-left: ' + (-PLAYER_PAWN_SIZE / 2) + 'px;' +
         '';
     playerPawn.setAttribute('style', playerPawnStyle);
 
     playerPawn.onmouseover = function (e) {
-        var explainingDiv = document.createElement('div');
-        explainingDiv.setAttribute('class', 'explaining')
-        explainingDiv.setAttribute('id', player.player + '_explaining_div');
+        var otherDiv = document.getElementById(player.player + '_explaining_div');
+        if (otherDiv == null) {
+            var explainingDiv = document.createElement('div');
+            explainingDiv.setAttribute('class', 'explaining');
+            explainingDiv.setAttribute('id', player.player + '_explaining_div');
 
-        var explainingDivTitle = document.createElement('h1');
-        explainingDivTitle.innerHTML = player.player;
-        explainingDiv.appendChild(explainingDivTitle);
+            //adding title label
+            var explainingDivTitle = document.createElement('h1');
+            explainingDivTitle.innerHTML = player.player;
+            explainingDiv.appendChild(explainingDivTitle);
 
-        var explainingDivStyle = '' +
-            'top: ' + player.position_y + 'px;' +
-            'left: ' + (parseInt(player.position_x) + PLAYER_PAWN_SIZE) + 'px;' +
-            '';
-        explainingDiv.setAttribute('style', explainingDivStyle);
+            //adding position label
+            var explainingDivTitle = document.createElement('p');
+            explainingDivTitle.innerHTML = "(x=" + player.position_x + ", y=" + player.position_y + ")";
+            explainingDiv.appendChild(explainingDivTitle);
 
-        map.appendChild(explainingDiv);
+            var explainingDivStyle = '' +
+                'top: ' + (player.position_y - PLAYER_PAWN_SIZE) + 'px;' +
+                'left: ' + (parseInt(player.position_x) + PLAYER_PAWN_SIZE / 2) + 'px;' +
+                '';
+            explainingDiv.setAttribute('style', explainingDivStyle);
+
+            map.appendChild(explainingDiv);
+        }
     }
 
     playerPawn.onmouseleave = function (e) {
@@ -61,16 +74,61 @@ function drawPlayer(player) {
         }
     }
 
+    playerPawn.onmouseup = function (e) {
+        if (PLAYER_FOLLOWER == null || PLAYER_FOLLOWER != player.player) {
+            PLAYER_FOLLOWER = player.player;
+        } else {
+            PLAYER_FOLLOWER = null;
+        }
+    }
+
     map.appendChild(playerPawn);
+
+    //Check if this is the followed player
+    if (PLAYER_FOLLOWER == player.player) {
+        //follower circle
+        var follower = document.createElement('div');
+        follower.setAttribute('id', 'follower');
+
+        var followerStyle = '' +
+            'top: ' + (player.position_y) + 'px;' +
+            'left: ' + (player.position_x) + 'px;' +
+            '';
+        follower.setAttribute('style', followerStyle);
+
+        map.appendChild(follower);
+
+        //follower text
+        var followerText = document.createElement('p');
+        followerText.setAttribute('id', 'follower_text');
+        followerText.innerHTML = player.player;
+
+        var followerTextStyle = '' +
+            'top: ' + parseInt(player.position_y) + 'px;' +
+            'left: ' + (player.position_x) + 'px;' +
+            '';
+        followerText.setAttribute('style', followerTextStyle);
+        map.appendChild(followerText);
+    }
 }
 
 function drawPlayers(players) {
-    // remove all pawns
-    var pawns = document.getElementsByClassName('pawn');
+    // remove all pawns and the follower (if present)
+    var pawns = document.getElementById('map_div').getElementsByClassName('pawn');
     if (pawns !== undefined) {
-        for (i in pawns) {
-            pawns[i].parentNode.removeChild();
+        for (i = 0; i < pawns.length; i++) {
+            pawns[i].remove();
         }
+    }
+
+    var follower = document.getElementById('follower');
+    if (follower != null) {
+        follower.remove();
+    }
+
+    var followerText = document.getElementById('follower_text');
+    if (followerText != null) {
+        followerText.remove();
     }
 
     //draw each player
